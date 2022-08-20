eaf4dd64e6f7072e44b5d8c492a9c3912f682842
eftekin <75522456+eftekin@users.noreply.github.com>
2022-08-20 22:26:48 +0300

    Initial Commit

---
+++ QuickAbdest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuickAbdest
+//
+//  Created by Mustafa Eftekin on 20.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
