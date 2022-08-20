fe037c86b0416ce1e9a313bc817442f1363b8566
eftekin <75522456+eftekin@users.noreply.github.com>
2022-08-20 23:52:39 +0300

    first commit

--- QuickAbdest/ViewController.swift
+++ QuickAbdest/ViewController.swift
@@ -6,13 +6,41 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var feelsLabel: UILabel!
+    @IBOutlet weak var feelsButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+   
     }
+    let feels = UIImage(named: "feels.png")
+    
+    var soundeffect: AVAudioPlayer?
+    
+    @IBAction func feelsButton(_ sender: UIButton) {
+        
+        
+        feelsButton.setImage(feels, for: .normal)
+        feelsLabel.text = ("A B D E S T L E N D I N")
+
+        let path = Bundle.main.path(forResource: "soundeffect.mp3", ofType:nil)!
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            soundeffect = try! AVAudioPlayer(contentsOf: url)
+            soundeffect?.play()
+        } catch {
+            print("opps...")
+        }
+        
+        }
+    
+
+
 
 
 }
