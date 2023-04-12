70b24412130caf0c39ffe7266e8667056981247d
eftekin <75522456+eftekin@users.noreply.github.com>
2023-04-12 09:59:32 +0300

    Initial Commit

---
+++ QuickAbdestRemastered/QuickAbdestRemasteredApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickAbdestRemasteredApp.swift
+//  QuickAbdestRemastered
+//
+//  Created by Mustafa Eftekin on 12.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct QuickAbdestRemasteredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
