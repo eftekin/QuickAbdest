cd8e989f8455990af980b71ab56af51207d7665e
eftekin <75522456+eftekin@users.noreply.github.com>
2023-04-12 12:08:21 +0300

    change app name and add button to reset abdest

--- QuickAbdestRemastered/ContentView.swift
+++ QuickAbdestRemastered/ContentView.swift
@@ -2,58 +2,76 @@ import SwiftUI
 import AVFoundation
 
 struct ContentView: View {
-    @State private var showImageOne = true
-    @State private var showLabel = false
+    @State private var showFeels = false
+    @State private var showText = false
     @State private var audioPlayer: AVAudioPlayer?
     
     var body: some View {
         ZStack {
-            Color.black
-                .edgesIgnoringSafeArea(.all)
-
+            Color.black.edgesIgnoringSafeArea(.all)
+            
             VStack {
-                Image(showImageOne ? "feelsbad" : "feels")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 200, height: 200)
-                    .onTapGesture {
-                        if showImageOne {
-                            showImageOne.toggle()
-                            showLabel = true
-                            playSound()
+                Spacer()
+                VStack {
+                    if showFeels {
+                        Image("feels")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 200, height: 200)
+                    } else {
+                        Image("feelsbad")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 200, height: 200)
+                    }
+                    
+                    if showText {
+                        Text("A B D E S T L E N D I N")
+                            .font(.custom("Gill Sans Bold", size: 23))
+                            .foregroundColor(Color.red)
+                    }
+                }
+                .onTapGesture {
+                    if !showFeels {
+                        showFeels.toggle()
+                        showText.toggle()
+                        
+                        if let audioFile = Bundle.main.path(forResource: "soundeffect", ofType: "mp3") {
+                            let audioURL = URL(fileURLWithPath: audioFile)
+                            audioPlayer = try? AVAudioPlayer(contentsOf: audioURL)
+                            audioPlayer?.play()
+                            
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 20) {
+                                showFeels.toggle()
+                                showText.toggle()
+                            }
                         }
                     }
-                if showLabel {
-                    Text("A B D E S T L E N D I N")
-                        .font(.custom("GillSans-Bold", size: 23))
-                        .foregroundColor(.red)
-                        .padding()
+                }
+                
+                Spacer()
+            }
+            
+            if showFeels {
+                VStack {
+                    HStack {
+                        Spacer()
+                        
+                        Image(systemName: "arrow.clockwise")
+                            .foregroundColor(Color.gray)
+                            .padding(.trailing)
+                            .font(.system(size: 32))
+                        
+                    }
+                    
+                    Spacer()
+                }
+                .onTapGesture {
+                    showFeels = false
+                    showText = false
+                    audioPlayer?.stop()
                 }
             }
         }
-        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
-            audioPlayer?.stop()
-        }
-    }
-    
-    func playSound() {
-        if let soundURL = Bundle.main.url(forResource: "sound", withExtension: "mp3") {
-            do {
-                audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
-                audioPlayer?.play()
-                DispatchQueue.main.asyncAfter(deadline: .now() + (audioPlayer?.duration ?? 20)) {
-                    showImageOne = true
-                    showLabel = false
-                }
-            } catch {
-                print("Error loading audio file")
-            }
-        }
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
